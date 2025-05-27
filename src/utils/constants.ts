7b4373c0bb04c9dbc2aebeaf59481d6331cae319
James Shih <9959168+jameshih@users.noreply.github.com>
2025-05-27 13:57:52 +0800

    Updated polkadot/api version and wss url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const WS_URL = "wss://statemint-rpc.dwellir.com";
+export const WS_URL = "wss://polkadot-asset-hub-rpc.polkadot.io";
 export const POLKADOT_ASSET_HUB = 0;
 export const DECIMAL = 1000000;
 export const USDC = { ASSET_ID: 1337, DECIMAL: 1000000 };
